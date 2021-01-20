6382ff18d9cd515348beb3606755d760543d7875
pharaoh111 <ammon.pro@gmail.com>
2021-01-20 20:50:34 +0100

    check_00

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,7 @@ require 'dotenv'# Appelle la gem Dotenv
 
 
 
-Dotenv.load('../.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
+Dotenv.load #('../.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
 # et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
@@ -33,7 +33,7 @@ client.update('Test Tweet W/ API, check_01')
 
 
 # Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
-puts ENV['TWITTER_API_SECRET']
+#puts ENV['TWITTER_API_SECRET']
 
 #Autre exemple
-puts ENV['BEST_WEBSITE_EVER']
+#puts ENV['BEST_WEBSITE_EVER']
